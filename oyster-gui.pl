293e5ff892ac1db3ca246f219bb33f2c966c22da
Benjamin Hanzelmann <ben@nabcos.de>
2004-04-12 16:28:38 +0000

    *** empty log message ***

--- oyster-gui.pl
+++ oyster-gui.pl
@@ -1,8 +1,25 @@
 #!/usr/bin/perl
 use CGI qw/:standard/;
+
+open(INFO, "/tmp/oyster/info");
+$info = <INFO>;
+$info = $info . "<br>";
+close(INFO);
+
+my $title = "";
+
+if ($info =~ /mp3$/) {
+    @output = `id3v2 -l $info`;
+    foreach my $line (@output) {
+	if ($line =~ /^Title/) {
+	    ($title) = m/:\ (.*)$/;
+	}
+    }
+}
+
 print header,
     start_html('Oyster-GUI'),
-    h1('Oyster-GUI'),
+    h1('Oyster-GUI spielt $title'),
     a({href=>'skip.pl'},'Skip'),
     end_html;
 
