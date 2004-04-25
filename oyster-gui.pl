06554dc171f79e81ea0c97a76b506d07ac79c8ba
Stephan Windmüller <windy@white-hawk.de>
2004-04-25 10:01:36 +0000

    - CSS - id3v1-Tags - OGG-Tags - Stop-Funktion

--- oyster-gui.pl
+++ oyster-gui.pl
@@ -9,7 +9,7 @@ close(INFO);
 
 my ($title, $artist) = "";
 
-print header, start_html('Oyster-GUI');
+print header, start_html(-title=>'Oyster-GUI',-style=>{'src'=>'layout.css'});;
 
 if ($info =~ /mp3$/) {
     open (MP3, "id3v2 -l \"$info\"|") or die $!;
@@ -18,23 +18,56 @@ if ($info =~ /mp3$/) {
     foreach my $line (@output) {
 	if ($line =~ /^Title/) {
 	    $_ = $line;
-	    ($title) = m/:\ (.*)$/;
+	    if ($line =~ /^Title\ \ \:.*Artist/) {
+		# id3v1
+		($title,$artist) = m/^Title\ \ \:\ (.*)Artist\:\ (.*)/;
+		$title =~ s/[\ ]*$//;
+	    } else {
+		# id3v2
+		($title) = m/:\ (.*)$/;
+	    }
 	} elsif ($line =~ /^Lead/) {
 	    $_ = $line;
 	    ($artist) = m/:\ (.*)$/;
 	}
-	
+    }	
+
+    close (MP3);
+
+} elsif ($info =~ /ogg$/) {
+    open (OGG, "ogginfo \"$info\"|") or die $!;
+    my @output = <OGG>;
+    
+    foreach my $line (@output) {
+	$line =~ s/^\w*//;
+	$line =~ s/TITLE=/title=/;
+	$line =~ s/ARTIST=/artist=/;
+	if ($line =~ /title=/) {
+	    $_ = $line;
+	    ($title) = m/title=(.*)/;
+	} elsif ($line =~ /artist=/) {
+	    $_ = $line;
+	    ($artist) = m/artist=(.*)/;
+	}
     }
+
+    close (OGG);
+    
 }
+
+
 
 if ($title eq "") {
     $title = $info;
     $title =~ s@.*/@@;
+} else {
+    $title = "$artist - $title";
 }
 
 print
-    h1("$artist - $title"),
+    h1("<a href='fileinfo.pl?file=$info' target='browse'>$title</a>"),
     a({href=>'skip.sh'},'Skip'),p,
-    a({href=>'oyster-gui.pl'},'Refresh'),
+    a({href=>'oyster-gui.pl'},'Refresh'),p,
+    a({href=>'stop.sh'},'Stop'),
     end_html;
 
