eac8a8b5196b74efe12c53509fe0b77421c2e59a
Stephan Windmüller <windy@white-hawk.de>
2004-05-01 10:30:08 +0000

    - Tag-Informationen werden jetzt per taginfo.pm geholt - Hinweis, wenn Oyster noch nicht gestartet wurde - Entfernung von "hardcoded directories"

--- oyster-gui.pl
+++ oyster-gui.pl
@@ -3,6 +3,7 @@ use CGI qw/:standard -no_xhtml/;
 use URI::Escape;
 use strict;
 use oyster::conf;
+use oyster::taginfo;
 
 my %config = oyster::conf->get_config('oyster.conf');
 
@@ -13,6 +14,9 @@ if (param('action')) {
 	print CONTROL 'NEXT';
 	close CONTROL;
 	sleep 1;
+    } elsif ($action eq 'start') {
+	system("perl oyster.pl &");
+	sleep 5;
     }
 }
 
@@ -32,9 +36,19 @@ print
 	       -head=>CGI::meta({-http_equiv => 'Content-Type',
 				 -content    => 'text/html; charset=iso-8859-1'}));
 
-print "<meta http-equiv='refresh' content='30; URL=oyster-gui.pl'>";
-
 my $basedir = $config{'basedir'};
+
+print h1('Oyster');
+print "<a href='oyster-gui.pl' style='position:absolute; top:2px; right:2px'><img src='refresh.gif' border='0'></a>";
+
+if (!(-e "$basedir")) {
+    print "<p>Oyster has not been started yet!</p>";
+    print "<p><a href='oyster-gui.pl?action=start'>Start</a></p>";
+    print end_html;
+    exit 0;
+}
+
+print "<meta http-equiv='refresh' content='30; URL=oyster-gui.pl'>";
 
 open(INFO, "$basedir" . "info");
 my $info = <INFO>;
@@ -42,92 +56,31 @@ chomp($info);
 $info =~ s/^np:\ //;
 close(INFO);
 
-my ($title, $artist) = "";
-
-if ($info =~ /mp3$/) {
-    open (MP3, "id3v2 -l \"$info\"|") or die $!;
-    my @output = <MP3>;
-
-    foreach my $line (@output) {
-	if ($line =~ /^Title/) {
-	    $_ = $line;
-	    if ($line =~ /^Title\ \ \:.*Artist/) {
-		# id3v1
-		($title,$artist) = m/^Title\ \ \:\ (.*)Artist\:\ (.*)/;
-		$title =~ s/[\ ]*$//;
-	    } else {
-		# id3v2
-		($title) = m/:\ (.*)$/;
-	    }
-	} elsif ($line =~ /^Lead/) {
-	    $_ = $line;
-	    ($artist) = m/:\ (.*)$/;
-	}
-    }	
-
-    close (MP3);
-
-} elsif ($info =~ /ogg$/) {
-    open (OGG, "ogginfo \"$info\"|") or die $!;
-    my @output = <OGG>;
-    
-    foreach my $line (@output) {
-	$line =~ s/^\w*//;
-	$line =~ s/TITLE=/title=/;
-	$line =~ s/ARTIST=/artist=/;
-	if ($line =~ /title=/) {
-	    $_ = $line;
-	    ($title) = m/title=(.*)/;
-	} elsif ($line =~ /artist=/) {
-	    $_ = $line;
-	    ($artist) = m/artist=(.*)/;
-	}
-    }
-
-    close (OGG);
-    
-}
-
-
-
-if ($title eq "") {
-    $title = $info;
-    $title =~ s@.*/@@;
-    $title =~ s/\.mp3//;
-    $title =~ s/\.ogg//;
-} else {
-    $title = "$artist - $title";
-}
+my %tag = oyster::taginfo->get_tag($info);
 
 $info = uri_escape("$info", "^A-Za-z");
 
-print h1('Oyster');
-print "<a href='oyster-gui.pl' style='position:absolute; top:2px; right:2px'><img src='refresh.gif' border='0'></a>";
 print "<table width='100%'>";
-print "<tr><td><strong>Now playing: <a class='file' href='fileinfo.pl?file=$info' target='browse'>$title</a></strong></td>";
+print "<tr><td><strong>Now playing: <a class='file' href='fileinfo.pl?file=$info' target='browse'>$tag{'display'}</a></strong></td>";
 print "<td><a href='oyster-gui.pl?action=skip'>Skip</a></td></tr>";
 print "</table>";
 
-open (VOTES, '/tmp/oyster/votes');
+open (VOTES, "$basedir" . 'votes');
 my @votes = <VOTES>;
 
-if (-s '/tmp/oyster/votes') {
+if (-s "$basedir" . 'votes') {
     print "<table width='100%' style='margin-top:3em;'><tr>";
     print "<th width='70%' align='left'>Voted File</th><th align='center'>Num of votes</th>";
     foreach my $vote (@votes) {
 	chomp ($vote);
 	my ($numvotes, $title);
 	$_ = $vote;
-	($title, $numvotes) = m@.*\/(.*),(.*)@;
-	$title =~ s/\.mp3$//;
-	$title =~ s/\.ogg$//;
-	my $escapedvote = $vote;
-	$escapedvote =~ s/,[1-9]*$//;
-	$escapedvote = uri_escape("$escapedvote", "^A-Za-z");
-	print "<tr><td><a class='file' href='fileinfo.pl?file=$escapedvote' target='browse'>$title</a></td><td align='center'>$numvotes</td></tr>\n";
+	($title, $numvotes) = m@(.*),([0-9]*)@;
+	%tag = oyster::taginfo->get_tag($title);
+	my $escapedtitle = uri_escape("$title", "^A-Za-z");
+	print "<tr><td><a class='file' href='fileinfo.pl?file=$escapedtitle' target='browse'>$tag{'display'}</a></td><td align='center'>$numvotes</td></tr>\n";
     }
     print "</table>";
 }
 
 print end_html;
-
