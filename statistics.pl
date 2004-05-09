2e84842aefb01df1c248866e004d5c39705038b3
Stefan Naujokat <git@ethric.de>
2004-05-09 12:29:03 +0000

    windy_axx: - Benutzung von oyster::common - "Some numbers"

--- statistics.pl
+++ statistics.pl
@@ -4,28 +4,19 @@ use URI::Escape;
 use strict;
 use oyster::conf;
 use oyster::taginfo;
+use oyster::common;
 
 my %config = oyster::conf->get_config('oyster.conf');
 
-print
-    header,
-    start_html(-title=>'Oyster-GUI',
-	       -style=>{'src'=>"themes/${config{'theme'}}/layout.css"},
-	       -head=>CGI::meta({-http_equiv => 'Content-Type',
-				 -content    => 'text/html; charset=iso-8859-1'}));
-
-print "<table width='100%'><tr>";
-print "<td align='center' width='20%'><a href='browse.pl'>Browse</a></td>";
-print "<td align='center' width='20%'><a href='search.pl'>Search</a></td>";
-print "<td align='center' width='20%'><a href='blacklist.pl'>Blacklist</a></td>";
-print "<td align='center' width='20%'><a href='score.pl'>Scoring</a></td>";
-print "<td align='center' width='20%'><a href='statistics.pl'>Statistics</a></td>";
-print "</tr></table>";
-print "<hr>";
+oyster::common->navigation_header();
 
 my %config = oyster::conf->get_config('oyster.conf');
 
 my @mostplayed = get_mostplayed();
+my $votedfiles = get_numfiles('VOTED');
+my $randomfiles = get_numfiles('PLAYLIST');
+my $scoredfiles = get_numfiles('LASTVOTES');
+my $totalfilesplayed = $votedfiles + $randomfiles + $scoredfiles;
 
 print h1('Most played songs');
 
@@ -82,66 +73,24 @@ foreach my $line (@lastplayed) {
 
 }
 
-exit 0;
-
-
-
-
-
-
-
-
-
-
-
-
-
-open (LOG, "${config{'savedir'}}log");
-
-my $next = '';
-my $cssclass = 'file2';
-
-print "<table>";
-
-while ((!($next eq '')) || (my $line = <LOG>)) {
-    if (!($next eq '')) {
-	$line = $next;
-	$next = '';
-    }
-    my ($year, $month, $day, $hour, $minute, $second, $playreason, $filename);
-    chomp($line);
-    $_ = $line;
-    ($year, $month, $day, $hour, $minute, $second, $playreason, $filename) =
-	m@^([0-9]{4})([0-9]{2})([0-9]{2})\-([0-9]{2})([0-9]{2})([0-9]{2})\ ([^\ ]*)\ (.*)$@;
-    if (($playreason eq 'PLAYLIST') || ($playreason eq 'LASTVOTES') || ($playreason eq 'VOTED')) {
-	do {
-	    $line = <LOG>;
-	} while ($line =~ /^[0-9]{8}\-[0-9]{6}\ [UN]{0,2}PAUSED\ /);
-	chomp($line);
-	$_ = $line;
-	my ($endreason, $filename2) = '';
-	($endreason, $filename2) = m@^[0-9]{8}\-[0-9]{6}\ ([^\ ]*)\ (.*)$@;
-	my $display = oyster::taginfo->get_tag_light($filename);
-	if ($cssclass eq 'file') {
-	    $cssclass = 'file2';
-	} else {
-	    $cssclass = 'file';
-	}
-	if (!($endreason eq '')) {
-	    $endreason = " / $endreason";
-	}
-	$filename =~ s/^\Q$config{'mediadir'}\E//;
-	$filename = "/$filename";
-	my $escapedfilename = uri_escape("$filename", "^A-Za-z");
-	print "<tr><td>$playreason$endreason</td>";
-	print "<td><a class='$cssclass' href='fileinfo.pl?file=$escapedfilename'>$display</a></td></tr>\n";
-	if (!($filename eq $filename2)) {
-	    $next = $line;
-	}
-    }
-}
-
 print "</table>";
+
+print h1('Some numbers');
+
+my $totalfiles = `wc -l lists/default`;
+$totalfiles =~ /^[\ ]*([0-9]*)/;
+$totalfiles = $1;
+
+print "<table width='100%'>";
+print "<tr><td><strong>Total files in playlist</strong></td><td>$totalfiles</td></tr>";
+print "<tr><td><strong>Total files played</strong></td><td>$totalfilesplayed</td></tr>";
+print "<tr><td><strong>Files played because of vote</strong></td><td>$votedfiles</td></tr>";
+print "<tr><td><strong>Files played because of scoring</strong></td><td>$scoredfiles</td></tr>";
+print "<tr><td><strong>Files played from playlist at random</strong></td><td>$randomfiles</td></tr>";
+print "<tr><td><strong>Ratio Scoring/Random (should be ~ $config{'voteplay'})</td>";
+print "<td>" . int(($scoredfiles/($scoredfiles+$randomfiles)*100)) . "</td></tr>";
+print "</table>";
+
 print end_html;
 
 exit 0;
@@ -163,6 +112,10 @@ sub get_mostplayed {
 	($year, $month, $day, $hour, $minute, $second, $playreason, $filename) =
 	    m@^([0-9]{4})([0-9]{2})([0-9]{2})\-([0-9]{2})([0-9]{2})([0-9]{2})\ ([^\ ]*)\ (.*)$@;
 	if (($playreason eq 'PLAYLIST') || ($playreason eq 'LASTVOTES') || ($playreason eq 'VOTED')) {
+	    $totalfilesplayed++;
+	    if ($playreason eq 'VOTED') {
+		$votedfiles++;
+	    }
 	    if ($timesplayed{$filename}) {
 		$timesplayed{$filename}++;
 		$maxplayed++ if ($timesplayed{$filename} > $maxplayed);
@@ -226,5 +179,25 @@ sub get_lastplayed {
     }
 
     return @lastplayed;
+
+}
+
+sub get_numfiles {
+
+    my $numfiles = 0;
+    my $type = $_[0];
+
+    open (LOG, "${config{'savedir'}}log");
+    while (my $line = <LOG>) {
+        my ($year, $month, $day, $hour, $minute, $second, $playreason, $filename);
+        chomp($line);
+        $_ = $line;
+        ($year, $month, $day, $hour, $minute, $second, $playreason, $filename) =
+            m@^([0-9]{4})([0-9]{2})([0-9]{2})\-([0-9]{2})([0-9]{2})([0-9]{2})\ ([^\ ]*)\ (.*)$@;
+	$numfiles++ if ($playreason eq $type);
+    }
+    close LOG;
+
+    return $numfiles;
 
 }
