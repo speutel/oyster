7fa8a4407fcc71c4d87d9c54f42063337ecc697c
Benjamin Hanzelmann <ben@nabcos.de>
2004-05-10 16:44:42 +0000

    - set table width to 75 pixel (?)

--- oyster-gui.pl
+++ oyster-gui.pl
@@ -66,7 +66,7 @@ if ($status eq 'paused') {
 }
 
 print "<table width='100%' border='0'>";
-print "<tr><td><strong>Now playing:</strong></td><td align='center' width='65'><strong>Score</strong></td></tr>";
+print "<tr><td><strong>Now playing:</strong></td><td align='center' width='75'><strong>Score</strong></td></tr>";
 print "<tr><td><strong><a class='file' href='fileinfo.pl?file=$info' target='browse'>$display</a>$statusstr</strong></td>";
 print "<td align='center' style='padding-left:10px; padding-right:10px'><a href='oyster-gui.pl?action=scoredown&amp;file=$info'><img src='themes/${config{'theme'}}/scoredownfile.png' border='0' alt='-'></a> ";
 print "<strong>$tag{'score'}</strong> ";
