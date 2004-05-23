bbb57afb565f9ed85c45a819d9abeda9f38fc026
Stefan Naujokat <git@ethric.de>
2004-05-23 22:19:03 +0000

    added Playlists menu entry

--- oyster/common.pm
+++ oyster/common.pm
@@ -17,11 +17,12 @@ sub navigation_header {
 					       -content    => 'text/html; charset=iso-8859-1'}));
 
     print "<table width='100%'><tr>";
-    print "<td align='center' width='20%'><a href='browse.pl'>Browse</a></td>";
-    print "<td align='center' width='20%'><a href='search.pl'>Search</a></td>";
-    print "<td align='center' width='20%'><a href='blacklist.pl'>Blacklist</a></td>";
-    print "<td align='center' width='20%'><a href='score.pl'>Scoring</a></td>";
-    print "<td align='center' width='20%'><a href='statistics.pl'>Statistics</a></td>";
+    print "<td align='center' width='17%'><a href='browse.pl'>Browse</a></td>";
+    print "<td align='center' width='16%'><a href='search.pl'>Search</a></td>";
+    print "<td align='center' width='17%'><a href='playlists.pl'>Playlists</a></td>";
+    print "<td align='center' width='17%'><a href='blacklist.pl'>Blacklist</a></td>";
+    print "<td align='center' width='16%'><a href='score.pl'>Scoring</a></td>";
+    print "<td align='center' width='17%'><a href='statistics.pl'>Statistics</a></td>";
     print "</tr></table>";
     print "<hr>";
 
