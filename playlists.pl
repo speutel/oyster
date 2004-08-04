909293892c7333b7f58c33742b15b534d0dd3c33
Stephan Windmüller <windy@white-hawk.de>
2004-08-04 07:49:48 +0000

    Playlists k�nnen nun angelegt, editiert und gel��scht werden

--- playlists.pl
+++ playlists.pl
@@ -13,6 +13,19 @@ my $playlist = oyster::conf->get_playlist();
 
 oyster::common->navigation_header();
 
+if (param('action') && param('listname')) {
+    if (param('action') eq 'addnewlist') {
+	my $newlist = param('listname');
+	$newlist =~ s/.*\///;
+	open (NEWLIST, ">$config{savedir}lists/$newlist");
+	close (NEWLIST);
+    } elsif (param('action') eq 'delete') {
+	my $dellist = param('listname');
+	$dellist =~ s/.*\///;
+	unlink("$config{savedir}lists/$dellist");
+    }
+}
+
 my $globdir = "${savedir}lists/";
 my @entries = <$globdir*>;
 
@@ -25,17 +38,34 @@ foreach my $entry (@entries) {
     }
 }
 
-print "<table width='100%'>";
+print "<table width='100%' style='margin-bottom: 2em;'>";
 
 foreach my $file (@files) {
     if ($file eq $playlist) {
 	print "<tr><td><i>$file</i></td><td>currently playing</td>";
     }
     else {
-	print "<tr><td>$file</td><td><a href='oyster-gui.pl?action=loadlist&amp;file=$file' target='curplay'>Load List</a></td>";
+	print "<tr><td>$file</td>" .
+	    "<td><a href='oyster-gui.pl?action=loadlist&amp;file=$file'" .
+	    "target='curplay'>Load List</a></td>";
     }
+    print "<td><a href='editplaylist.pl?action=edit&amp;" .
+	"playlist=$file'>Edit List</a></td>\n";
+    print "<td><a href='playlists.pl?action=delete&amp;" .
+	"listname=$file'>Delete List</a></td>\n";
+
 }
 
 print "</table>";
+
+print start_form;
+
+print hidden(-name=>'action', -default=>'addnewlist');
+
+print textfield(-name=>'listname',-default=>'');
+print submit(-value=>'Create new list',-style=>'margin-left: 2em;');
+
+print end_form;
+
 
 print end_html;
